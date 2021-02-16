9451b7741f56d6bf4b6c4de47bdbcbc60e71e4ae
tichise <43707+tichise@users.noreply.github.com>
2021-02-16 22:22:29 +0900

    Create AnglePoint.swift

---
+++ Sources/OMPitchAndRoll/AnglePoint.swift
@@ -0,0 +1,16 @@
+//
+//  AnglePoint.swift
+//  
+//
+//  Created by tichise on 2021年2月16日 21/02/16.
+//
+
+import SwiftUI
+
+struct AnglePoint: View {
+    var body: some View {
+        Circle()
+            .fill(Color.red)
+            .frame(width: 10, height: 10)
+    }
+}

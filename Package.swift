a4f8bef9830bab9b641a2ec7ba884a23ff03d805
tichise <43707+tichise@users.noreply.github.com>
2021-02-16 23:50:51 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -27,5 +27,6 @@ let package = Package(
         .testTarget(
             name: "OMPitchAndRollTests",
             dependencies: ["OMPitchAndRoll"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )

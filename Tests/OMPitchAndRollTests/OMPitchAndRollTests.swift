85faa1e8a1e9a6d3860cb6b16e572d450e4e3511
tichise <43707+tichise@users.noreply.github.com>
2021-02-16 22:22:19 +0900

    Update OMPitchAndRollTests.swift

--- Tests/OMPitchAndRollTests/OMPitchAndRollTests.swift
+++ Tests/OMPitchAndRollTests/OMPitchAndRollTests.swift
@@ -3,13 +3,5 @@ import XCTest
 
 final class OMPitchAndRollTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(OMPitchAndRoll().text, "Hello, World!")
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
